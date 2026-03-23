3c25aef0ee00d4879a64282afda7ce1c8342e72e
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-23 14:02:44 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
     // TODO
 
+    int value, maxVal;
+
+    cin >> maxVal;
+    for(int i = 0; i < 9; i++){
+        cin >> value;
+        if (value > maxVal){
+            maxVal = value;
+        }
+    }
+    cout << maxVal;
+    
     return 0;
 }
